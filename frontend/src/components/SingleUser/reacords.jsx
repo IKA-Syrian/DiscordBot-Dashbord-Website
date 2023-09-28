421204647541be268323d8c3cb41f3a3b7d6f914
I.K.A Syrian <computertk2018@gmail.com>
2023-09-28 21:25:38 +0300

    change records layout

--- frontend/src/components/SingleUser/reacords.jsx
+++ frontend/src/components/SingleUser/reacords.jsx
@@ -37,114 +37,143 @@ export function GetRecords({ history, guildID, userRecord }) {
     // return data.id == data.id ? <UpdateData singleData={singleData}/> : data
   };
   switch (guildID) {
-    // case "699320430934098030" :
-    //     return (
-    //         <>
-    //             <tr>
-    //                 <th>ID</th>
-    //                 <th>User ID</th>
-    //                 <th>User Name</th>
-    //                 <th>Series ShortName</th>
-    //                 <th>Chapter Number</th>
-    //                 <th>Position</th>
-    //                 <th>Adding Points</th>
-    //                 <th>Adding Balance</th>
-    //                 <th>Date</th>
-    //                 <th>Update</th>
-    //                 <th>Delete</th>
-    //             </tr>
-    //             {
-    //                 userRecord.map((data) => (
-    //                     <tr>
-    //                         <td>{data.id}</td>
-    //                         <td>{data.member_id}</td>
-    //                         <td>{Buffer.from(data.member).toString().trim()}</td>
-    //                         <td>{data.project_name}</td>
-    //                         <td>{data.chapter_number}</td>
-    //                         <td>{data.role}</td>
-    //                         <td>{data.adding_points}</td>
-    //                         <td>{data.adding_balance}  </td>
-    //                         <td>{converttolocal(data.date)}</td>
-    //                         <td><Button onClick={updateData1(data)}>Update</Button></td>
-    //                         <td><Button onClick={deleteData(data.id)}>Delete</Button></td>
-    //                         <br/>
-    //                     </tr>
-    //                 ))
-    //             }
-    //         </>
-    //     )
-    // case "777493917440016405":
-    //     return (
-    //         <>
-    //             <tr>
-    //                 <th>ID</th>
-    //                 <th>User ID</th>
-    //                 <th>User Name</th>
-    //                 <th>Series ShortName</th>
-    //                 <th>Chapter Number</th>
-    //                 <th>Position</th>
-    //                 <th>Adding Points</th>
-    //                 <th>Page Counts</th>
-    //                 <th>Date</th>
-    //                 <th>Update</th>
-    //                 <th>Delete</th>
-    //             </tr>
-    //             {
-    //                 userRecord.map((data) => (
-    //                     <tr>
-    //                         <td>{data.id}</td>
-    //                         <td>{data.member_id}</td>
-    //                         <td>{Buffer.from(data.member).toString().trim()}</td>
-    //                         <td>{data.project_name}</td>
-    //                         <td>{data.chapter_number}</td>
-    //                         <td>{data.role}</td>
-    //                         <td>{data.adding_points}</td>
-    //                         <td>{data.page_count}</td>
-    //                         <td>{converttolocal(data.date)}</td>
-    //                         <td><Button onClick={setSingleData(data).then(() => updateData1(data.id))}>Update</Button></td>
-    //                         <td><Button onClick={setSingleData(data).then(() => deleteData(data.id))}>Delete</Button></td>
-    //                         <br/>
-    //                     </tr>
-    //                 ))
-    //             }
-    //         </>
-    //     )
-    // case "770652852871888916":
-    //     return (
-    //         <>
-    //             <tr>
-    //                 <th>ID</th>
-    //                 <th>User ID</th>
-    //                 <th>User Name</th>
-    //                 <th>Series ShortName</th>
-    //                 <th>Chapter Number</th>
-    //                 <th>Position</th>
-    //                 <th>Adding Points</th>
-    //                 <th>Page Counts</th>
-    //                 <th>Date</th>
-    //                 <th>Update</th>
-    //                 <th>Delete</th>
-    //             </tr>
-    //             {
-    //                 userRecord.map((data) => (
-    //                     <tr>
-    //                         <td>{data.id}</td>
-    //                         <td>{data.member_id}</td>
-    //                         <td>{Buffer.from(data.member).toString().trim()}</td>
-    //                         <td>{data.project_name}</td>
-    //                         <td>{data.chapter_number}</td>
-    //                         <td>{data.role}</td>
-    //                         <td>{data.adding_points}</td>
-    //                         <td>{data.page_count}</td>
-    //                         <td>{converttolocal(data.date)}</td>
-    //                         <td><Button onClick={setSingleData(data).then(() => updateData1(data.id))}>Update</Button></td>
-    //                         <td><Button onClick={setSingleData(data).then(() => deleteData(data.id))}>Delete</Button></td>
-    //                         <br/>
-    //                     </tr>
-    //                 ))
-    //             }
-    //         </>
-    //     )
+    case "699320430934098030":
+      return (
+        <>
+          <tr>
+            <th>ID</th>
+            <th>User ID</th>
+            <th>User Name</th>
+            <th>Series ShortName</th>
+            <th>Chapter Number</th>
+            <th>Position</th>
+            <th>Adding Points</th>
+            <th>Adding Balance</th>
+            <th>Date</th>
+            <th>Update</th>
+            <th>Delete</th>
+          </tr>
+          {userRecord.map((data) => (
+            <tr>
+              <td>{data.id}</td>
+              <td>{data.member_id}</td>
+              <td>{Buffer.from(data.member).toString().trim()}</td>
+              <td>{data.project_name}</td>
+              <td>{data.chapter_number}</td>
+              <td>{data.role}</td>
+              <td>{data.adding_points}</td>
+              <td>{data.adding_balance} </td>
+              <td>{converttolocal(data.date)}</td>
+              <td>
+                <Button
+                  id={RecordData.id}
+                  onClick={() => updateData1(RecordData.id)}
+                >
+                  Update
+                </Button>
+              </td>
+              {/* <td>
+                <Button onClick={deleteData(data.id)}>Delete</Button>
+              </td> */}
+              <br />
+            </tr>
+          ))}
+        </>
+      );
+    case "777493917440016405":
+      return (
+        <>
+          <tr>
+            <th>ID</th>
+            <th>User ID</th>
+            <th>User Name</th>
+            <th>Series ShortName</th>
+            <th>Chapter Number</th>
+            <th>Position</th>
+            <th>Adding Points</th>
+            <th>Page Counts</th>
+            <th>Date</th>
+            <th>Update</th>
+            <th>Delete</th>
+          </tr>
+          {userRecord.map((data) => (
+            <tr>
+              <td>{data.id}</td>
+              <td>{data.member_id}</td>
+              <td>{Buffer.from(data.member).toString().trim()}</td>
+              <td>{data.project_name}</td>
+              <td>{data.chapter_number}</td>
+              <td>{data.role}</td>
+              <td>{data.adding_points}</td>
+              <td>{data.page_count}</td>
+              <td>{converttolocal(data.date)}</td>
+              <td>
+                <Button
+                  id={RecordData.id}
+                  onClick={() => updateData1(RecordData.id)}
+                >
+                  Update
+                </Button>
+              </td>
+              {/* <td>
+                <Button
+                  onClick={setSingleData(data).then(() => deleteData(data.id))}
+                >
+                  Delete
+                </Button>
+              </td> */}
+              <br />
+            </tr>
+          ))}
+        </>
+      );
+    case "770652852871888916":
+      return (
+        <>
+          <tr>
+            <th>ID</th>
+            <th>User ID</th>
+            <th>User Name</th>
+            <th>Series ShortName</th>
+            <th>Chapter Number</th>
+            <th>Position</th>
+            <th>Adding Points</th>
+            <th>Page Counts</th>
+            <th>Date</th>
+            <th>Update</th>
+            <th>Delete</th>
+          </tr>
+          {userRecord.map((data) => (
+            <tr>
+              <td>{data.id}</td>
+              <td>{data.member_id}</td>
+              <td>{Buffer.from(data.member).toString().trim()}</td>
+              <td>{data.project_name}</td>
+              <td>{data.chapter_number}</td>
+              <td>{data.role}</td>
+              <td>{data.adding_points}</td>
+              <td>{data.page_count}</td>
+              <td>{converttolocal(data.date)}</td>
+              <td>
+                <Button
+                  id={RecordData.id}
+                  onClick={() => updateData1(RecordData.id)}
+                >
+                  Update
+                </Button>
+              </td>
+              {/* <td>
+                <Button
+                  onClick={setSingleData(data).then(() => deleteData(data.id))}
+                >
+                  Delete
+                </Button>
+              </td> */}
+              <br />
+            </tr>
+          ))}
+        </>
+      );
     default:
       return (
         <>
